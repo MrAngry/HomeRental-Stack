af9036203b2af68d95d780587fc340e85144122c
damian <kb.damian@gmail.com>
2020-04-28 00:59:18 +0200

    Create basic models foundation

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+from django.db.models import Model, CharField, DecimalField, DateTimeField, ForeignKey, DO_NOTHING
+
+
+class Contract(Model):
+
+    def sum(self):
+        return sum(payment.value for payment in self.items.all())
+
+
+class Payment(Model):
+    description = CharField(max_length=200, blank=True)
+    value = DecimalField(max_digits=8,decimal_places=2, null=False)
+    createdAt = DateTimeField(auto_now_add=True)
+    updatedAt = DateTimeField(auto_now_add=True)
+    contract = ForeignKey(to=Contract, on_delete=DO_NOTHING,related_name='items')
---
+++ payments/test_models.py
@@ -0,0 +1,17 @@
+import random
+from decimal import Decimal
+
+from django.test import TestCase
+
+from payments.models import Contract, Payment
+
+
+class TestContract(TestCase):
+    def test_sum(self):
+        # Generate random payments value
+        values = list(Decimal(random.randrange(0, 100)) / 100 + Decimal(random.randrange(0, 2000)) for i in range(30))
+
+        contract = Contract.objects.create()
+        payments = list(Payment.objects.create(value=value, contract=contract) for value in values)
+
+        self.assertEquals(sum(values), contract.sum(),msg="Summing method failed")
